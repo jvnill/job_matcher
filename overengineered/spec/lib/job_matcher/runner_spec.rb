ee79c8a7372606f977a5aaf9f6f384529b9c5c27
Jim Ruther Nill <jvnill@gmail.com>
2024-08-04 22:24:09 +1000

    add specs for job_matcher/runner.rb

---
+++ overengineered/spec/lib/job_matcher/runner_spec.rb
@@ -0,0 +1,85 @@
+describe JobMatcher::Runner do
+  let(:options) { double }
+  let(:jobseekers) { [] }
+  let(:jobs) { [] }
+  let(:input) { double(jobseekers: jobseekers, jobs: jobs) }
+  let(:output) { double }
+  let(:runner) { JobMatcher::Runner.new(options) }
+
+  before do
+    expect(JobMatcher::Input).to receive(:get_input).with(options).and_return(input)
+    expect(JobMatcher::Output).to receive(:get_output).with(options).and_return(output)
+  end
+
+  context 'no input' do
+    it 'should output nothing' do
+      expect(output).to receive(:output).with([])
+      runner.process
+    end
+  end
+
+  context 'single jobseeker' do
+    let(:jobseekers) do
+      [{
+        id: 1,
+        name: 'Peter Parker',
+        skills: ['Ruby', 'SQL']
+      }]
+    end
+
+    context 'no match' do
+      let(:jobs) do
+        [{
+          id: 1,
+          title: 'Java Platform Engineer',
+          required_skills: ['Java', 'Kubernetes']
+        }]
+      end
+
+      before do
+        expect_any_instance_of(JobMatcher::SkillMatcher).to receive(:process).and_return([])
+      end
+
+      it 'should output nothing' do
+        expect(output).to receive(:output).with(
+          [{
+            jobseeker: jobseekers[0],
+            job_matches: []
+          }]
+        )
+
+        runner.process
+      end
+    end
+
+    context 'with match' do
+      let(:jobs) do
+        [{
+          id: 1,
+          title: 'Ruby Platform Engineer',
+          required_skills: ['Ruby', 'Kubernetes']
+        }]
+      end
+
+      before do
+        expect_any_instance_of(JobMatcher::SkillMatcher).to receive(:process).and_call_original
+      end
+
+      it 'should output nothing' do
+        expect(output).to receive(:output).with(
+          [{
+            jobseeker: jobseekers[0],
+            job_matches: [{
+              job: jobs[0],
+              matching_skills: ['Ruby'],
+              matching_skills_count: 1,
+              matching_skills_percentage: 50
+            }]
+          }]
+        )
+
+        runner.process
+      end
+    end
+  end
+end
